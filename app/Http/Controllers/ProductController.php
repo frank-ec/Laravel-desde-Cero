bf2b616af9ea02dc1e36988835fac8599750d6db
frank-ec <franklynlara@hotmail.com>
2022-09-30 15:49:31 -0500

    Control de acceso por sesion

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -8,6 +8,12 @@ use Illuminate\Support\Facades\DB;
 
 class ProductController extends Controller
 {
+    // Si no inicia sesion redirecciona
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+ 
     public function index()
     {   // $products  = DB::table('products')->get(); //Query Builder
         //dd($products );
