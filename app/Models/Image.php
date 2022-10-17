d945ce2b39862fab057dec96d73febb9e4788803
frank-ec <franklynlara@hotmail.com>
2022-10-17 18:17:07 -0500

    hasta 54

--- app/Models/Image.php
+++ app/Models/Image.php
@@ -7,5 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Image extends Model
 {
-    use HasFactory;
+    //use HasFactory;
+     /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'path',
+    ];
 }
